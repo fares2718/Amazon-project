79b12581ea3eac6f9617eb923508985d556ffa05
fares2718 <faresobaid2715@gmail.com>
2025-08-02 23:17:52 +0300

    Added money test using jasmine

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+    it("converts cents into dollars", () => {
+        expect(formatCurrency(2095)).toEqual("20.95");
+    });
+
+    it("Works with 0", () => {
+        expect(formatCurrency(0)).toEqual("0.00");
+    });
+
+    it("Round up to the nearest cent", () => {
+        expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+
+    it("Round down to the nearest cent", () => {
+        expect(formatCurrency(2000.2)).toEqual("20.00");
+    });
+});
